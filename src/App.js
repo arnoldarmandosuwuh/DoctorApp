c66b0ba8ea3533fbca2e9be6843b33767ecd7fdd
Arnold Armando Suwuh <arnoldarmando07@gmail.com>
2020-10-01 08:40:07 +0700

    fix log warning

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native'
 import React from 'react'
-import { YellowBox } from 'react-native'
+import { LogBox } from 'react-native'
 import FlashMessage from 'react-native-flash-message'
 import { Provider, useSelector } from 'react-redux'
 import { Loading } from './components'
@@ -9,7 +9,7 @@ import Router from './router'
 
 const MainApp = () => {
     const stateGlobal = useSelector((state) => state)
-    YellowBox.ignoreWarnings(['Setting a timer'])
+    LogBox.ignoreLogs(['Setting a timer'])
     return (
         <>
             <NavigationContainer>
