f50cb4f3f6025ef94e851167562cb0445d8166a9
Arnold Armando Suwuh <arnoldarmando07@gmail.com>
2020-10-01 08:43:53 +0700

    finish list hospital

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native'
 import React from 'react'
-import { LogBox } from 'react-native'
+import { YellowBox } from 'react-native'
 import FlashMessage from 'react-native-flash-message'
 import { Provider, useSelector } from 'react-redux'
 import { Loading } from './components'
@@ -9,7 +9,7 @@ import Router from './router'
 
 const MainApp = () => {
     const stateGlobal = useSelector((state) => state)
-    LogBox.ignoreLogs(['Setting a timer'])
+    YellowBox.ignoreWarnings(['Setting a timer'])
     return (
         <>
             <NavigationContainer>
